b59e84d56f6d5a8db47e83d1e368ce17bb32bd33
ryderwsos <86503143+ryderwsos@users.noreply.github.com>
2023-12-14 19:31:02 -0500

    added social icon animations and links

---
+++ src/assets/LinkedInIcon.jsx
@@ -0,0 +1,7 @@
+export const LinkedInIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" strokeWidth="2.5" stroke="#CACBD2" fill="none" className="duration-300 transform transition-all" >
+    <path d="M34.34 24.1h-8.69v31.43h9v-14.4c0-3.82.72-7.49 5.43-7.49s4.72 4.34 4.72 7.73v14.16h9V39.46c0-7.76-1.65-14.92-10.81-14.92-3.38-.13-6.65.49-8.47 3.25a.11.11 0 01-.21-.06zM10.92 24.1H20v31.43h-9.08zM15.44 8.47a5.27 5.27 0 105.25 5.29v0a5.25 5.25 0 00-5.25-5.29z" strokeLinecap="round"></path>
+    </svg>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
 import './Home.css'
 import { GithubIcon } from '../assets/GithubIcon'
-import { EmailIcon } from '../assets/EmailIcon'
+import { LinkedInIcon } from '../assets/LinkedInIcon'
 
 export const Home = () => {
+
   return (
     <>
         <div className='main-info'>
@@ -12,8 +13,11 @@ export const Home = () => {
                 <p className='section-header' style={{opacity:0.8}}>About me:</p> 
                 <p className='section-content' style={{opacity:0.5}}>I'm currently a student attending the University of Waterloo on my 2A term studying Electrical Engineering. </p>
             </div>
-            <div className='icon'>
-              <GithubIcon />
+            <div 
+              className='icon'
+            >
+              <a href='https://github.com/ryderwsos' target="_blank"  rel="noopener noreferrer" className='socialItem'><GithubIcon /></a>
+              <a href='https://www.linkedin.com/in/ryderwang' target="_blank"  rel="noopener noreferrer" className='socialItem'><LinkedInIcon /></a>
             </div>
         </div>
     </>
